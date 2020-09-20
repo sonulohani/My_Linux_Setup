75f1d0fd2d82745f7e6ab1c22bfddea69f262b76
Sonu Lohani <sonulohani@gmail.com>
2020-09-20 18:58:45 +0530

    Add zsh plugin script

--- install.sh
+++ install.sh
@@ -59,23 +59,8 @@ echo "Install ohmyzsh"
 cd ${HOME}
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Installing zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo "Installing zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-echo "Installing powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Extra
-# ZSH Manually setup----------------------------------------------------
-# ZSH_THEME="powerlevel10k/powerlevel10k"
-# plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
-# My changes begin                                                              
-# export PATH=${HOME}/Programs:$PATH                                          
-# My changes end  
-#------------------------------------------------------------------------
 # Install chrome
 # Install code
 # Install virtualbox

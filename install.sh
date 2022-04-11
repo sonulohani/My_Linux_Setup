8ebce32b34ce19e1209ec3746f7d4da9c3a43db1
Sonu Lohani <sonulohani@gmail.com>
2022-04-11 10:14:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,18 +20,19 @@ echo "Installing essential packages"
 sudo apt install -y python3-pip build-essential binutils neovim cmake-qt-gui \
 gufw g++ gdb git ubuntu-restricted-extras rar unrar p7zip-full p7zip-rar neofetch \
 htop silversearcher-ag xclip meld obs-studio apt-transport-https curl gnome-tweaks \
-gnome-shell-extensions wget network-manager-openvpn-gnome extra-cmake-modules tlp \
-smartmontools mesa-common-dev libglu1-mesa-dev vlc flatpak gimp zsh ninja-build gettext \
-libtool libtool-bin autoconf automake cmake g++ pkg-config unzip fsearch
+gnome-shell-extensions wget network-manager-openvpn-gnome extra-cmake-modules \
+mesa-common-dev libglu1-mesa-dev vlc flatpak gimp zsh ninja-build gettext \
+libtool libtool-bin autoconf automake cmake g++ pkg-config unzip fsearch fonts-hack-ttf
 
 # echo "Installing optional packages"
 # sudo apt install -y dconf-editor timeshift terminator snapd flatpak gimp qt5-default zsh 
 # sudo apt install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 
 # Fonts
+https://github.com/tonsky/FiraCode
 https://github.com/adobe-fonts/source-code-pro
-https://github.com/microsoft/cascadia-code
-https://github.com/dharmatype/Sometype-Mono
+https://www.nerdfonts.com/font-downloads
+https://github.com/madmalik/mononoki/tree/master
 
 # Configure flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -46,40 +47,13 @@ echo "Installing Spacevim"
 # https://spacevim.org/quick-start-guide/
 curl -sLf https://spacevim.org/install.sh | bash
 
-echo "Copying fonts"
-mkdir -p ${HOME}/.local/share/fonts
-cp -r ${SCRIPT_DIR}/fonts/* ${HOME}/.local/share/fonts
-fc-cache -fv
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
-
 echo "Enabling firewall"
 sudo systemctl enable ufw
-
-echo "Installing terminator theme plugin"
-pip3 install requests
-mkdir -p $HOME/.config/terminator/plugins
-wget https://git.io/v5Zww -O $HOME"/.config/terminator/plugins/terminator-themes.py"
 
 echo "Setting ssh"
 ssh-keygen -t rsa -b 4096 -C "sonulohani@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ${HOME}/.ssh/id_rsa
-
-# echo "Installing starship"
-# curl -fsSL https://starship.rs/install.sh | bash
-
-# Add this in bash
-# eval "$(starship init bash)"
-
-echo "Installing Copyq"
-sudo add-apt-repository ppa:hluk/copyq
-sudo apt update
-sudo apt install copyq
-
-echo "Installing peek"
-sudo add-apt-repository ppa:peek-developers/stable
-sudo apt update
-sudo apt install peek
 
 echo "Install ohmyzsh"
 cd ${HOME}

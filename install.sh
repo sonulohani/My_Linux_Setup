c5f7e0e4f1c326e7e410c6e2ab21ef2cbcf361b9
Sonu Lohani <sonulohani@gmail.com>
2022-04-11 10:18:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ echo "Installing essential packages"
 sudo apt install -y python3-pip build-essential binutils neovim cmake-qt-gui \
 gufw g++ gdb git ubuntu-restricted-extras rar unrar p7zip-full p7zip-rar neofetch \
 htop silversearcher-ag xclip meld obs-studio apt-transport-https curl gnome-tweaks \
-gnome-shell-extensions wget network-manager-openvpn-gnome extra-cmake-modules \
+gnome-shell-extensions wget network-manager-openvpn-gnome extra-cmake-modules timeshift \
 mesa-common-dev libglu1-mesa-dev vlc flatpak gimp zsh ninja-build gettext \
 libtool libtool-bin autoconf automake cmake g++ pkg-config unzip fsearch fonts-hack-ttf
 

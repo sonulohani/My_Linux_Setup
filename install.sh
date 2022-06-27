a28272fbcc33a5eae18f821175414cc8b3dfb8b6
Sonu Lohani <sonulohani@gmail.com>
2022-06-27 09:37:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,6 @@ libtool libtool-bin autoconf automake cmake g++ pkg-config unzip fsearch fonts-h
 
 # deb-get
 curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
-
 deb-get install google-chrome-stable code
 
 # Snap refresh

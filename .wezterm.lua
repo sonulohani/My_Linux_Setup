640ce365ab95f461c8b92b8dc76a47561120b46b
Sonu Lohani <sonulohani@gmail.com>
2023-07-06 09:43:47 +0530

    Add files via upload

---
+++ .wezterm.lua
@@ -0,0 +1,25 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Gruvbox dark, medium (base16)'
+config.enable_scroll_bar = true
+config.font = wezterm.font('CaskaydiaCove Nerd Font')
+config.font_size = 11.0
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
+
+-- and finally, return the configuration to wezterm
+return config
+

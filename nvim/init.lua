29066ea67b517f560c31c1ec9eab5cdd32c80f2a
Sonu Lohani <sonulohani@gmail.com>
2023-08-04 09:42:31 +0000

    Added neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+vim.loader.enable()
+
+require('settings')
+require('plugins')
+require('keybinds')
+require('mason-config')
---
+++ nvim/lua/plugins/lsp_config_setup.lua
@@ -0,0 +1,8 @@
+-- Setup language servers.
+local lspconfig = require('lspconfig')
+lspconfig.clangd.setup {}
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)

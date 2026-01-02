04d6f5f63cb4b835bf5e8bc7b227abcfacb90cb0
sonulohani <sonulohani@gmail.com>
2026-01-02 18:58:42 +0530

    Fix nvim directory tracking - convert from submodule to normal files

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+local keymap = vim.keymap.set
+
+keymap("n", "<leader>d", '"_d', { noremap = true, silent = true })
+keymap("x", "<leader>d", '"_d', { noremap = true, silent = true })
+keymap("x", "<leader>p", '"_dP', { noremap = true, silent = true })
+
+-- Normal Mode (Move single line)
+keymap("n", "<A-S-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+keymap("n", "<A-S-Up>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- Visual Mode (Move selected block)
+-- 'v' mode covers both Visual and Visual-Line modes
+keymap("v", "<A-S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+keymap("v", "<A-S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Disable relative numbering
+vim.opt.relativenumber = false
+
+-- Ensure absolute numbering is on
+vim.opt.number = true

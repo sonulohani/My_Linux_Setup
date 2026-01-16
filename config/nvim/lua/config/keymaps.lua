ea6a9d432ea99fbf855f691272e11cc9da7e9f45
sonulohani <sonulohani@gmail.com>
2026-01-16 10:45:26 +0530

    Refactor keymaps to simplify movement commands by removing Shift modifier

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -9,10 +9,10 @@ keymap("x", "<leader>d", '"_d', { noremap = true, silent = true })
 keymap("x", "<leader>p", '"_dP', { noremap = true, silent = true })
 
 -- Normal Mode (Move single line)
-keymap("n", "<A-S-Down>", ":m .+1<CR>==", { desc = "Move line down" })
-keymap("n", "<A-S-Up>", ":m .-2<CR>==", { desc = "Move line up" })
+keymap("n", "<A-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+keymap("n", "<A-Up>", ":m .-2<CR>==", { desc = "Move line up" })
 
 -- Visual Mode (Move selected block)
 -- 'v' mode covers both Visual and Visual-Line modes
-keymap("v", "<A-S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-keymap("v", "<A-S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })

cab51a67a6b4cdf890183b6859f3cde2f403a3e6
sonulohani <sonulohani@gmail.com>
2026-09-03 17:16:45 +0530

    Add noctalia config

---
+++ config/hypr_noctalia/config/variables.lua
@@ -0,0 +1,15 @@
+-- Hyprland default apps
+
+TERMINAL     = "kitty"
+FILE_MANAGER = "nautilus"
+BROWSER      = "brave"
+EDITOR       = "gnome-text-editor --new-window"
+CALCULATOR   = "gnome-calculator"
+
+-- Monitors
+MONITOR1 = "HDMI-A-1"
+MONITOR2 = "eDP-1"
+PRIMARY_MONITOR = MONITOR1
+
+-- Workspaces
+NUM_WPM = 5 -- Number of workspaces per monitor (Max 10)
---
+++ config/hypr_noctalia/hyprland.lua
@@ -0,0 +1,17 @@
+-- CachyOS Hyprland Configuration
+
+require("config.animations")
+require("config.autostart")
+require("config.colors")
+require("config.decorations")
+require("config.variables")
+require("config.environment")
+require("config.inputs")
+require("config.binds")
+require("config.misc")
+require("config.monitors")
+require("config.windowrules")
+require("config.workspaces")
+
+-- HyprMod managed settings
+require("hyprland-gui")
